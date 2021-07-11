91802b16bb0759c256e09aa4f7b21d4c6b54cfea
hakkisabah <hakkisabah@hotmail.com>
2021-07-11 21:34:02 +0300

    layouts ready

---
+++ Vue-Nuxt-HakkiSabah/layouts/default.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-app id="inspire">
+    <Navigation />
+    <v-main class="grey lighten-2">
+      <v-container>
+        <nuxt />
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script lang='ts'>
+import Vue from 'vue'
+import Navigation from '~/components/Navigation.vue'
+
+export default Vue.extend({
+  components: {
+    Navigation
+  }
+  // asyncData(ctx: Context): Promise<object | void> | object | void {
+  //
+  // }
+})
+</script>
