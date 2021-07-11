386e64a65eb7e333f7f2683f9b4c5f709d423b6e
hakkisabah <hakkisabah@hotmail.com>
2021-07-11 21:34:45 +0300

    pages ready

---
+++ Vue-Nuxt-HakkiSabah/pages/checkout.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-row>
+    <v-col v-if="$accessor.cart.cartItems && $accessor.cart.cartItems.length > 0">
+      <v-sheet
+        min-height="70vh"
+        rounded="lg"
+      >
+        <Cartlist />
+      </v-sheet>
+    </v-col>
+    <v-col v-else>
+      <v-card min-width="250" align="center">
+        Your Cart is Empty
+        {{ returnHome() }}
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
+
+<script lang='ts'>
+import Vue from 'vue'
+import Cartlist from '~/components/Cartlist.vue'
+
+export default Vue.extend({
+  name: 'Checkout',
+  components: { Cartlist },
+  methods: {
+    returnHome (): void {
+      this.$router.push('/')
+    }
+  }
+})
+</script>
+
+<style scoped>
+
+</style>
